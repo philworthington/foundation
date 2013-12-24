6ad50b63a09187f0d67e60cf9ca84bcf6133b86e
Phil Worthington <pworthington27@gmail.com>
2013-12-23 22:47:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Foundation
+
+By [Phil Worthington](https://github.com/philworthington).
+
+
+## Description
+
+
+## Authors
+
+* Phil Worthington (https://github.com/philworthington)
+
+
+## License
+
+MIT: http://philworthington.mit-license.org
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foundation::Application.config.secret_token = 'e0fedee61cfe3972371fb64d28c05372ebcf8710571c6060f5cfac5e51fbd3c874e288b24f4c45e09bba6f23648e1e5445c844ef452d21ffcb53dba4d3251e6f'
